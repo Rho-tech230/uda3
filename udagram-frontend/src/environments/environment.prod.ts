6f1c798e937eeb3f5031fd2cb970ced0fad9e462
Rho-tech230 <bamerhoda@gmail.com>
2022-11-27 00:26:36 +0100

    exposing the frontend

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a153da9aaabaf42948628a3db9936d44-1051899259.us-east-2.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
